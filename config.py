5a561bf554dacb1cc39bf68747244bb78761d606
Léo Jason Coulais <leo.jascoulais@gmail.com>
2026-07-12 15:08:10 +0200

    import admins.json only if exists

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ PASSWORD = os.getenv('PASSWORD')
 DEVICE_NAME = os.getenv('USER_ID')
 CREDENTIALS_FILE = "credentials.json"
 STORE_PATH = "./store"
+ADMIN_FILE="admins.json"
 
-with open("admins.json") as f:
-    ADMINS = set(json.load(f))
+if os.path.exists(ADMIN_FILE):
+    with open("admins.json") as f:
+        ADMINS = set(json.load(f))

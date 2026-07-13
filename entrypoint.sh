c64affc4a6e8bdbdc2530024b7396599d6408c69
Léo Jason Coulais <leo.jascoulais@gmail.com>
2026-07-13 18:07:47 +0200

    fix entrypoint bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 ollama serve &
+OLLAMA_PID=$!
 
 until ollama list > /dev/null 2>&1; do
     echo "Waiting for ollama..."
     sleep 1
 done
 
-#ollama pull llama3.1:8b
+trap 'kill -TERM $OLLAMA_PID $PYTHON_PID 2>/dev/null; wait' TERM INT
 
-python3 -u main.py
+python3 -u main.py &
+PYTHON_PID=$!
+wait $PYTHON_PID
